546a152b4759aa92532c4392402b0c86f021f2af
19893381 <MStaZakharov@sberbank.ru>
2024-08-05 16:46:15 +0300

    TTitle

--- src/common/TTitle.tsx
+++ src/common/TTitle.tsx
@@ -1,5 +1,5 @@
 import Title, {TitleProps} from "antd/es/typography/Title";
-import React, {FC, useMemo} from "react";
+import React, {FC, useEffect, useMemo} from "react";
 import useScroll from "./useScroll";
 import useWindowDimensions from "./useWindowDimensions";
 
@@ -7,11 +7,14 @@ const TTitle: FC<TitleProps> = (props) => {
     const {height, width, isMobile} = useWindowDimensions();
     const {yOffset} = useScroll();
 
-    const opacity = useMemo(() => isMobile ? (100 - yOffset * 2) / 100 : 1, [yOffset, isMobile]);
+    const opacity = useMemo(() => isMobile ? (58*2 - yOffset * 2) / 100 : 1, [yOffset, isMobile]);
+
+    // Ускорение появления мобильного хедера
+    const FIXED_HEADER_OPACITY_SPEED = 2;
 
     return <>
         <Title {...props} style={{opacity}}/>
-        <Title {...props} className="MobileHeader" style={{opacity: 0 - opacity}}/>
+        <Title {...props} className="MobileHeader" style={{opacity: 0 - opacity * FIXED_HEADER_OPACITY_SPEED}}/>
     </>
 }
 
